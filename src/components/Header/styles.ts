1696cd6a78eb3cbc356d06df551af48b3db3badc
Paulo Santana <paulosantana.sa@gmail.com>
2023-10-08 21:25:13 -0300

    feat: Add `Header` component in `Default Layout` .

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import headerImage from "../../assets/background.svg";
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  height: 18.5rem;
+  background: url(${headerImage}) no-repeat center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
