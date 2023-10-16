825f2aad95109269799dffaf783f21fe7950a3b4
Paulo Santana <paulosantana.sa@gmail.com>
2023-10-16 01:17:50 -0300

    feat: Integrate github API to fetch posts.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,55 @@
+import { useCallback, useEffect, useState } from "react";
+import { api } from "../../lib/axios";
 import { Post } from "./components/Post";
 import { Profile } from "./components/Profile";
 import { SearchInput } from "./components/SearchInput";
 import { PostsListContainer } from "./styled";
 
+const username = import.meta.env.VITE_GITHUB_USERNAME;
+const reponame = import.meta.env.VITE_GITHUB_REPONAME;
+
+export interface ResponsePost {
+  title: string;
+  body: string;
+  created_at: string;
+  number: number;
+  html_url: string;
+  comments: number;
+  user: {
+    login: string;
+  };
+}
+
 export function Home() {
+  const [posts, setPosts] = useState<ResponsePost[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getPosts = useCallback(async (query: string = "") => {
+    try {
+      setIsLoading(true);
+      const response = await api.get(
+        `/search/issues?q=${query}%20repo:${username}/${reponame}`
+      );
+
+      console.log(response.data);
+      setPosts(response.data.items);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
     <>
       <Profile />
       <SearchInput />
       <PostsListContainer>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        {posts.map((post) => (
+          <Post key={post.number} post={post} />
+        ))}
       </PostsListContainer>
     </>
   );
